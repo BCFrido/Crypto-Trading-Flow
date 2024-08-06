91312c5947e8858ddf9ceef482a32332e8aff3d8
Frido <fsd.slb.games@gmail.com>
2024-08-06 19:24:05 +0100

    fix hti 2.0

--- src/scenes/howtoinstall/hti.tsx
+++ src/scenes/howtoinstall/hti.tsx
@@ -8,7 +8,7 @@ const HowToInstallItems: HowToInstallItem[] = [
   {
     icon: "simple-icons:binance",
     title: "Binance",
-    link: "https://docs.google.com/document/d/1ofKdJnsmhuqDQ96cvlqAuPGTvgatCD8GG3KkYP1n3QU/edit",
+    link: "https://docs.google.com/document/d/e/2PACX-1vSAqcUpt3ko5ZOca32_Y-guDpX7e5J7eP12-VgGi3LOYKAGYCrqk6uZRzXPEzpXzEIApDIHWZW0dNLN/pub",
   },
 ];
 
